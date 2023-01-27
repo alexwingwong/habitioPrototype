97bb64a061fb52591a83a09ec17a385120a97828
Alex <wongalex2@gmail.com>
2023-01-26 22:41:56 -0700

    got day data saving to service

--- src/app/day-details/day-details.component.ts
+++ src/app/day-details/day-details.component.ts
@@ -5,11 +5,15 @@ import { Day, calendar } from '../day'
 
 import { CalandarManagerService } from '../calandar-manager.service';
 import { Subscription } from 'rxjs';
+import { defaultIfEmpty } from 'rxjs';
 
 @Component({
   selector: 'app-day-details',
   templateUrl: './day-details.component.html',
-  styleUrls: ['./day-details.component.css']
+  styleUrls: ['./day-details.component.css'],
+  host: {
+    role: ''
+  }
 })
 export class DayDetailsComponent implements OnInit, OnDestroy {
 
@@ -17,6 +21,7 @@ export class DayDetailsComponent implements OnInit, OnDestroy {
   day: Day;
   index: number;
   subscription: Subscription;
+  newDay: Day;
 
   constructor(private route: ActivatedRoute, private calendarManager: CalandarManagerService) {
 
@@ -29,6 +34,17 @@ export class DayDetailsComponent implements OnInit, OnDestroy {
     this.day = calendar[productIDFromRoute];
     this.index = this.day.monthIndex;
 
+    this.newDay = {
+      isDay: this.day.isDay,
+      dayNum: this.day.dayNum,
+      date: this.day.date,
+      monthIndex: this.day.monthIndex,
+      mood: this.day.mood,
+      sleep: this.day.sleep,
+      physicalActivity: this.day.physicalActivity,
+      cssClass: 'grey'
+    }
+
     this.subscription = this.calendarManager.publicDays.subscribe(calendar => this.calendar = calendar);
   }
 
@@ -37,17 +53,36 @@ export class DayDetailsComponent implements OnInit, OnDestroy {
   }
 
   updateDay() {
-    let newDay = {
-      isDay: this.day.isDay,
-      dayNum: this.day.dayNum,
-      date: this.day.date,
-      monthIndex: this.day.monthIndex,
-      mood: 2,
-      sleep: 2,
-      physicalActivity: 2,
-      cssClass: 'lightRed'
-    }
-    this.calendarManager.updateDay(newDay, newDay.monthIndex);
+
+    console.log(this.newDay)
+    this.calendarManager.updateDay(this.newDay, this.newDay.monthIndex);
   }
 
+  updateExercise(event: any) {
+    console.log("Exercise:", event.source.value)
+    this.newDay.physicalActivity = event.source.value;
+  }
+
+  updateSleep(event: any) {
+    console.log("Sleep:", event.source.value)
+    this.newDay.sleep = event.source.value;
+  }
+
+  updateMood(event: any) {
+    console.log("Mood:", event.source.value)
+    this.newDay.mood = event.source.value;
+
+    switch (event.source.value) {
+      case '1': this.newDay.cssClass = "darkRed"; break;
+      case '2': this.newDay.cssClass = "darkRed"; break;
+      case '3': this.newDay.cssClass = "lightRed"; break;
+      case '4': this.newDay.cssClass = "beige"; break;
+      case '5': this.newDay.cssClass = "lightGreen"; break;
+      case '6': this.newDay.cssClass = "darkGreen"; break;
+      case '7': this.newDay.cssClass = "darkGreen"; break;
+      default: this.newDay.cssClass = "grey"; break;
+    }
+
+    console.log(this.newDay.cssClass)
+  }
 }

a32bf44245f379efd793ea4279c31e7b7991406b
Alex <wongalex2@gmail.com>
2023-01-26 21:00:39 -0700

    service worked!!!

--- src/app/day-details/day-details.component.ts
+++ src/app/day-details/day-details.component.ts
@@ -1,26 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
-import {Day, calendar} from '../day'
+import { Day, calendar } from '../day'
+
+import { CalandarManagerService } from '../calandar-manager.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-day-details',
   templateUrl: './day-details.component.html',
   styleUrls: ['./day-details.component.css']
 })
-export class DayDetailsComponent {
+export class DayDetailsComponent implements OnInit, OnDestroy {
 
-  day: Day | undefined;
+  calendar: Day[]
+  day: Day;
+  index: number;
+  subscription: Subscription;
 
-  constructor(private route: ActivatedRoute){
+  constructor(private route: ActivatedRoute, private calendarManager: CalandarManagerService) {
 
   }
 
-  ngOnInit(){
+  ngOnInit() {
     const routeParams = this.route.snapshot.paramMap;
     const productIDFromRoute = Number(routeParams.get('dayIndex'))
 
-    this.day = calendar[productIDFromRoute];  
+    this.day = calendar[productIDFromRoute];
+    this.index = this.day.monthIndex;
+
+    this.subscription = this.calendarManager.publicDays.subscribe(calendar => this.calendar = calendar);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  updateDay() {
+    let newDay = {
+      isDay: this.day.isDay,
+      dayNum: this.day.dayNum,
+      date: this.day.date,
+      monthIndex: this.day.monthIndex,
+      mood: 2,
+      sleep: 2,
+      physicalActivity: 2,
+      cssClass: 'darkRed'
+    }
+    this.calendarManager.updateDay(newDay, newDay.monthIndex);
   }
 
 }
